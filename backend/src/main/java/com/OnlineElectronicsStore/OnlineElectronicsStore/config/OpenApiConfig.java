65a551552845c9fb8594cb12c841bac522b6aa7c
Vasyl Shcherbakov <vs.shcher.82@gmail.com>
2026-01-11 21:06:58 +0100

    Verified Registration and Tokens and Authentication_Swagger=0n

---
+++ backend/src/main/java/com/OnlineElectronicsStore/OnlineElectronicsStore/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.OnlineElectronicsStore.OnlineElectronicsStore.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+
+
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Online Store API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+
+
+
+}
