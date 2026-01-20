2ab9e68ebd0976cf290f780fc63cb8c7f89a3fff
Vasyl Shcherbakov <vs.shcher.82@gmail.com>
2026-01-20 16:19:20 +0100

    Separate_REST_Thymeleaf_Separate_FIXCart_Controller_Errors

--- backend/src/main/java/com/OnlineElectronicsStore/OnlineElectronicsStore/controller/web/CartController.java
+++ backend/src/main/java/com/OnlineElectronicsStore/OnlineElectronicsStore/controller/web/CartController.java
@@ -15,7 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
