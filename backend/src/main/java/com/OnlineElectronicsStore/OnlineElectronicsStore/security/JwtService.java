d1667c6812a5c576e4d6c210c4df703c5155ddaf
Vasyl Shcherbakov <vs.shcher.82@gmail.com>
2026-03-11 14:34:58 +0100

    Changes JwtService

--- backend/src/main/java/com/OnlineElectronicsStore/OnlineElectronicsStore/security/JwtService.java
+++ backend/src/main/java/com/OnlineElectronicsStore/OnlineElectronicsStore/security/JwtService.java
@@ -10,13 +10,13 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    // 🔑 Секретный ключ (в продакшене храни в application.properties)
+
     private static final String SECRET = "my-secret-key-12345";
 
-    // Срок действия токена — 24 часа
+
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
-    // 🧾 Создание токена
+
     public String generateToken(String username) {
         return JWT.create()
                 .withSubject(username)
@@ -25,14 +25,14 @@ public class JwtService {
                 .sign(Algorithm.HMAC256(SECRET));
     }
 
-    // ✅ Проверка токена
+
     public String validateToken(String token) {
         try {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
             DecodedJWT decodedJWT = verifier.verify(token);
-            return decodedJWT.getSubject(); // Возвращает username
+            return decodedJWT.getSubject(); // Повертає імя юзера
         } catch (Exception e) {
-            return null; // Если токен неверный или просрочен
+            return null; // Якщо токен просрочений
         }
     }
 }
